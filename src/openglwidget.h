e3f10e18403af6e838fdcbe23beb3ea67596ad18
azagaya <azagaya.games@gmail.com>
2019-03-13 14:02:59 -0300

    GML removed

--- src/openglwidget.h
+++ src/openglwidget.h
@@ -10,9 +10,6 @@
 #include <QOpenGLBuffer>
 #include <QPixmap>
 #include <QWheelEvent>
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
 
 class OpenGlWidget : public QOpenGLWidget, protected QOpenGLFunctions
 {
