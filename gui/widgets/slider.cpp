dce6ddad1968a714bddef0a5eb97218334639917
azagaya <azagaya.games@gmail.com>
2020-02-05 14:02:14 -0300

    Added disabled colors for custom slider

--- gui/widgets/slider.cpp
+++ gui/widgets/slider.cpp
@@ -46,8 +46,8 @@ void Slider::resizeEvent(QResizeEvent *ev) {
   QString num = QString::number(spin_box.value());
   QFontMetrics fm(spin_box.font());
   spin_box.resize(fm.horizontalAdvance(num)+15,20);
- // spin_box.move(0.5*width()*value()/maximum()-0.5*value()/maximum()*spin_box.width(), height()/2-spin_box.height()/2);
-//  spin_box.move(0.5*value()/maximum()*spin_box.width(), height()/2-spin_box.height()/2);
+  // spin_box.move(0.5*width()*value()/maximum()-0.5*value()/maximum()*spin_box.width(), height()/2-spin_box.height()/2);
+  //  spin_box.move(0.5*value()/maximum()*spin_box.width(), height()/2-spin_box.height()/2);
 }
 
 void Slider::sliderChange(QAbstractSlider::SliderChange change)
@@ -67,5 +67,8 @@ void Slider::paintEvent(QPaintEvent *ev) {
   QSlider::paintEvent(ev);
   QRect rect(1.0*(width()-12.0)*(value()-minimum())/(maximum()-minimum())+37.0*(maximum()-value())/(maximum()-minimum()),8,4,4);
   QPainter painter(this);
-  painter.fillRect(rect, QBrush(QColor(153,153,255)));
+  if (isEnabled())
+    painter.fillRect(rect, QBrush(QColor(153,153,255)));
+  else
+    painter.fillRect(rect, QBrush(QColor(200,200,200)));
 }
