1907521b2dc398c8ecd7416e56a5cb307f75dc31
azagaya <azagaya.games@gmail.com>
2021-10-18 20:09:08 -0300

    Change version number

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ QCoreApplication *createApplication(int &argc, char *argv[])
 int main(int argc, char *argv[])
 {
   QCoreApplication::setApplicationName("laigter");
-  QCoreApplication::setApplicationVersion("1.10.6-beta");
+  QCoreApplication::setApplicationVersion("1.10.7-beta");
   //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
